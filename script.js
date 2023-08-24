39d307feba55b0a6eb08b3ee1cbbac0c5ab84901
Dirección de Estrategias de Integración del Sistema de Salud <138637441+deissMS@users.noreply.github.com>
2023-08-24 15:37:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+function levenshtein(a, b) {
+    if (a.length === 0) return b.length;
+    if (b.length === 0) return a.length;
+
+    var matrix = [];
+
+    var i;
+    for (i = 0; i <= b.length; i++) {
+        matrix[i] = [i];
+    }
+
+    var j;
+    for (j = 0; j <= a.length; j++) {
+        matrix[0][j] = j;
+    }
+
+    for (i = 1; i <= b.length; i++) {
+        for (j = 1; j <= a.length; j++) {
+            if (b.charAt(i - 1) == a.charAt(j - 1)) {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            } else {
+                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1));
+            }
+        }
+    }
+
+    return matrix[b.length][a.length];
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     fetch('https://deissms.github.io/buscador_m/consolidado.json')
     .then(response => response.json())
@@ -30,8 +59,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 let categoriaNormalizada = eliminarAcentos(obj.categoria.toLowerCase());
 
                 if (valorBuscado !== "") {
-                    return nombreNormalizado.includes(valorBuscado) || categoriaNormalizada.includes(valorBuscado);
-                } else {
+                    if (nombreNormalizado.includes(valorBuscado) || categoriaNormalizada.includes(valorBuscado)) {
+                        return true;
+                    }
+                
+                    for (let i = 0; i <= nombreNormalizado.length - valorBuscado.length; i++) {
+                        const segmento = nombreNormalizado.substr(i, valorBuscado.length);
+                        if (levenshtein(segmento, valorBuscado) <= 4) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                 else {
                     return obj.categoria === valorCategoria;
                 }
             });
